6b9176067699923fbdeff010f02c5f88693c7a2a
Sado <50923618+Sado9898kz@users.noreply.github.com>
2020-10-30 21:23:16 +0600

    Update Runner.java

--- src/com/company/Runner.java
+++ src/com/company/Runner.java
@@ -22,13 +22,13 @@ public class Runner extends Thread{
         } catch (InterruptedException e) {
             System.out.println("Thread has been interrupted");
         }
-        if (index == 1) {
-            System.out.println(this.getName() + index + " берет палочку"
-                    + "\n" + this.getName() + index + " бежит к финишу");
-        } else {
-            System.out.println(this.getName() + index + " берет палочку"
-                + "\n" + this.getName() + index + " бежит к "
-                + this.getName() + (index - 1));
-        }
+//         if (index == 1) {
+//             System.out.println(this.getName() + index + " берет палочку"
+//                     + "\n" + this.getName() + index + " бежит к финишу");
+//         } else {
+//             System.out.println(this.getName() + index + " берет палочку"
+//                 + "\n" + this.getName() + index + " бежит к "
+//                 + this.getName() + (index - 1));
+//         }
     }
 }
